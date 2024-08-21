567d3c76fbaed4b11fd17a65f25af4db4516bfcd
haruka wada <022.wadaha@gmail.com>
2024-08-21 15:30:42 +0900

    検索機能追加

---
+++ .history/src/app/Models/Contact_20240819173155.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Contact extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'category_id',
+        'first_name',
+        'last_name',
+        'gender',
+        'email',
+        'tell',
+        'address',
+        'building',
+        'detail'
+    ];
+    protected $guarded = ['id'];
+
+    public function category() {
+        return $this->belongsTo('App\Models\Category');
+    }
+
+    public function scopeKeywordSearch($query, $keyword ) {
+        if(!empty($keywoed)) {
+            $query -> where('first_name', 'like', '%', '$keyword', '%');
+            $query => where
+        }
+    }
+}
